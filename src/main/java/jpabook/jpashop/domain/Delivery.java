cc090c5aeddf932dc59ad95ec4cb013f2b09f3e3
So hyounsoo <so5663@naver.com>
2023-04-29 21:57:30 +0900

    fetch, cascade

--- src/main/java/jpabook/jpashop/domain/Delivery.java
+++ src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -1,9 +1,8 @@
 package jpabook.jpashop.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.OneToOne;
+import javax.persistence.*;
+
+import static javax.persistence.FetchType.*;
 
 @Entity
 public class Delivery extends BaseEntity{
@@ -14,6 +13,7 @@ public class Delivery extends BaseEntity{
     private String street;
     private String zipcode;
     private  DeliveryStatus status;
-    @OneToOne(mappedBy = "delivery")
+
+    @OneToOne(mappedBy = "delivery", fetch = LAZY)
     private Order order;
 }
